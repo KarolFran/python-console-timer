bc60d79b0374ebf26947ed8a588d5c6dcf0c0a96
KarolFran <49308325+KarolFran@users.noreply.github.com>
2024-06-12 11:55:19 +0200

    Update timer1.py

--- timer1.py
+++ timer1.py
@@ -30,47 +30,55 @@ elif(hours == 0):
     while(sec <= minutes*60+seconds):
         
         if(sec%60==0):
-            print("Minutes: " + str(min))
+            timer = str(min)
+            print("                Minutes:",timer, end="\r")
             min+=1
         if(sec > 60):
             if(sec%60==0):
                 pass
             else:
-                print("Seconds: " + str(sec-((min-1)*60)))
+                timer = "Seconds: " + str(sec-((min-1)*60))
+                print(timer, end="\r")
             time.sleep(1)
         else:
             if(sec%60==0):
                 pass
             else:
-                print("Seconds: " + str(sec))
+                timer = "Seconds: " + str(sec)
+                print(timer, end="\r")
             time.sleep(1)
         sec+=1
 
 else:
     while(sec <= (hours*60*60 + minutes*60 + seconds)): 
         if(min>60 and sec%60==0):
-            print("Minutes: " + str(min-(60*(hour-1))))
+            timer = str(min-(60*(hour-1)))
             min+=1
         else:
             if(sec%60==0):
-                print("Minutes: " + str(min))
+                timer = "                Minutes: " + str(min)
+                print(timer, end="\r")
                 min+=1
         if(sec%3600==0):
-            print("Hours: " + str(hour))
+            timer = "                                Hours: " + str(hour)
+            print(timer, end="\r")
             hour+=1
             
         if(sec > 60):
             if(sec%60==0):
                 pass
             else:
-                print("Seconds: " + str(sec-((min-1)*60)))
+                timer = "Seconds: " + str(sec-((min-1)*60))
+                print(timer, end="\r")
             time.sleep(1)
         else:
             if(sec%60==0):
                 pass
             else:
-                print("Seconds: " + str(sec))
+                timer = "Seconds: " + str(sec)
+                print(timer, end="\r")
             time.sleep(1)
         sec+=1
+    
     
    
